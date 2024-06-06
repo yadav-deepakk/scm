c9ef1122ab0d84e1718c2d7cbe2f24c172961204
yadav-deepakk <deepakyadav53882@gmail.com>
2024-06-06 23:58:14 +0530

    added feature dark theme.

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,1 +1,33 @@
 console.log("script Invoked!!!")
+retainThemeOnPageChangeOrReload();
+document.querySelector("#toggle-theme-btn").addEventListener("click", themeChanger);
+
+function themeChanger() {
+    // 1:light, 0:dark
+    let currentTheme = localStorage.getItem("theme") ?? 1;
+    let themeText = document.querySelector("#toggle-theme-btn span");
+    let htmlTag = document.querySelector("html");
+
+    console.log("cuurent theme: " + currentTheme + " and text: " + themeText.innerHTML);
+    if (currentTheme == 1) {
+        htmlTag.classList.add("dark");
+        console.log("changing to dark theme");
+        localStorage.setItem("theme", 0);
+        themeText.innerHTML = "Light";
+    } else {
+        htmlTag.classList.remove("dark");
+        console.log("changing to light theme");
+        localStorage.setItem("theme", 1);
+        themeText.innerHTML = "Dark";
+    }
+}
+
+function retainThemeOnPageChangeOrReload() {
+    let currentTheme = localStorage.getItem("theme") ?? 1;
+    let themeText = document.querySelector("#toggle-theme-btn span");
+    let htmlTag = document.querySelector("html");
+    if (currentTheme == 0 && !htmlTag.classList.contains("dark")) {
+        htmlTag.classList.add("dark");
+        themeText.innerHTML = "Light";
+    }
+}
