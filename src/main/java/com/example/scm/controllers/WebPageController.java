b8a6f73bf13a7148cc036391d35694fc57ed2d6e
yadav-deepakk <deepakyadav53882@gmail.com>
2024-06-16 20:34:47 +0530

    changed restcontroller to controller.

--- src/main/java/com/example/scm/controllers/WebPageController.java
+++ src/main/java/com/example/scm/controllers/WebPageController.java
@@ -1,23 +1,20 @@
 package com.example.scm.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.scm.entities.User;
 import com.example.scm.enums.MessageType;
 import com.example.scm.enums.Provider;
-import com.example.scm.forms.LoginForm;
 import com.example.scm.forms.SignUpForm;
 import com.example.scm.models.Message;
 import com.example.scm.services.servicesImpl.UserServiceImpl;
@@ -25,7 +22,7 @@ import com.example.scm.services.servicesImpl.UserServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
-@RestController
+@Controller
 public class WebPageController {
 
 	@Autowired
@@ -35,66 +32,58 @@ public class WebPageController {
 
 	// index/home page
 	@RequestMapping({ "/", "/home" })
-	public ModelAndView home() {
+	public String home(Model model) {
 		log.info("Displaying Home Page.");
-		Map<String, String> attributes = new HashMap<>();
-		attributes.put("projectName", "SCM");
-		attributes.put("githubUsername", "yadav-deepak");
-		attributes.put("githubLink", "https://github.com/yadav-deepakk");
-		return new ModelAndView("home", attributes);
+		model.addAttribute("projectName", "SCM");
+		model.addAttribute("githubUsername", "yadav-deepak");
+		model.addAttribute("githubLink", "https://github.com/yadav-deepakk");
+		return "home";
 	}
 
 	// about
 	@RequestMapping("/about")
-	public ModelAndView about() {
+	public String about() {
 		log.info("Displaying about page.");
-		return new ModelAndView("about", new HashMap<>());
+		return "about";
 	}
 
 	// services
 	@RequestMapping("/services")
-	public ModelAndView services() {
+	public String services() {
 		log.info("Displaying services page.");
-		return new ModelAndView("services", new HashMap<>());
+		return "services";
 	}
 
 	// contact
 	@RequestMapping("/contact")
-	public ModelAndView contact() {
+	public String contact() {
 		log.info("Displaying contact page.");
-		return new ModelAndView("contact", new HashMap<>());
+		return "contact";
 	}
 
 	// login
 	@RequestMapping("/login")
-	public ModelAndView login() {
+	public String login() {
 		log.info("Displaying login page.");
-		ModelAndView modelAndView = new ModelAndView("login");
-		LoginForm loginFormData = new LoginForm();
-		modelAndView.addObject("loginFormData", loginFormData);
-		return modelAndView;
+		return "login";
 	}
 
 	// signup
 	@RequestMapping("/signup")
-	public ModelAndView signup() {
+	public String signup(Model model) {
 		log.info("Displaying signup page.");
-		SignUpForm signupFormData = new SignUpForm();
-		ModelAndView modelAndView = new ModelAndView("signup");
-		modelAndView.addObject("signupFormData", signupFormData);
-		return modelAndView;
+		model.addAttribute("signupFormData", new SignUpForm());
+		return "signup";
 	}
 
 	/*
-	 * ==========================================
-	 * Form Submit Handlers - singup and login
-	 * ==========================================
+	 * ===============================
+	 * SingUp Form Submit Handler
+	 * ===============================
 	 */
 	@RequestMapping(value = "/signup-handler", method = RequestMethod.POST)
-	public ModelAndView registerUserHandler(
-			@Valid @ModelAttribute("signupFormData") SignUpForm signUpFormData,
-			BindingResult rBindingResult,
-			HttpSession session) {
+	public ModelAndView registerUserHandler(@Valid @ModelAttribute("signupFormData") SignUpForm signUpFormData,
+			BindingResult rBindingResult, HttpSession session) {
 		log.info("Processing User Registration.");
 
 		String message = "Registration successful.";
@@ -102,17 +91,15 @@ public class WebPageController {
 
 		if (rBindingResult.hasErrors()) {
 			log.info("Form has errors");
-			Message msg = Message
-					.builder()
-					.messageContent("You have errors, please correct and resubmit!")
-					.messageType(MessageType.red)
-					.build();
+			Message msg = Message.builder().messageContent("You have errors, please correct and resubmit!")
+					.messageType(MessageType.red).build();
 			session.setAttribute("message", msg);
 			return new ModelAndView("/signup");
 		}
 
 		// save user to database if data is valid.
-		User user = User.builder()
+		User user = User
+				.builder()
 				.name(signUpFormData.getName())
 				.email(signUpFormData.getEmail())
 				.password(signUpFormData.getPassword())
@@ -124,21 +111,9 @@ public class WebPageController {
 
 		userService.saveUser(user);
 
-		Message msg = Message
-				.builder()
-				.messageContent(message)
-				.messageType(type)
-				.build();
+		Message msg = Message.builder().messageContent(message).messageType(type).build();
 		session.setAttribute("message", msg);
 		return new ModelAndView(new RedirectView("/signup"));
-	}
-
-	@RequestMapping(value = "/login-handler", method = RequestMethod.POST)
-	public ModelAndView loginUserHandler(@ModelAttribute LoginForm loginFormData) {
-		log.info("Processing User Login.");
-
-		RedirectView homePageRedirection = new RedirectView("home");
-		return new ModelAndView(homePageRedirection);
 	}
 
 }
