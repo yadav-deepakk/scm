f5329506dd6d42c14e2cc6ea2dfa340811ff23df
yadav-deepakk <deepakyadav53882@gmail.com>
2024-06-25 23:56:21 +0530

    Working on user profile.

---
+++ src/main/java/com/example/scm/controllers/RootController.java
@@ -0,0 +1,40 @@
+package com.example.scm.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.scm.entities.User;
+import com.example.scm.helper.Helper;
+import com.example.scm.services.servicesImpl.UserServiceImpl;
+
+@ControllerAdvice
+public class RootController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Autowired
+    private Helper helper;
+
+    org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(RootController.class);
+
+    @ModelAttribute
+    public void addLoggedInUserToModel(Authentication authentication, Model model) {
+        if (authentication == null)
+            return;
+
+        log.info("Adding user details to the model.");
+
+        String email = helper.getEmailFromAuthentication(authentication);
+        User user = userService.getUserByEmail(email).get();
+
+        log.info("{}", user);
+        log.info(user.getName());
+        log.info(user.getEmail());
+
+        model.addAttribute("loggedInUser", user);
+    }
+}
---
+++ src/main/java/com/example/scm/helper/Helper.java
@@ -0,0 +1,40 @@
+package com.example.scm.helper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Helper {
+
+    Logger log = LoggerFactory.getLogger(Helper.class);
+
+    public String getEmailFromAuthentication(Authentication authentication) {
+
+        if (authentication instanceof OAuth2AuthenticationToken) { // oauth login
+            var clientId = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+            var oauth2User = (OAuth2User) authentication.getPrincipal();
+            String username = null;
+            switch (clientId) {
+                case "google":
+                    username = oauth2User.getAttribute("email");
+                    break;
+                case "github":
+                    username = oauth2User.getAttribute("email") == null
+                            ? oauth2User.getAttribute("login") + "@gmail.com"
+                            : oauth2User.getAttribute("email");
+                    break;
+                default:
+                    log.error("unknown login method");
+                    break;
+            }
+            return username;
+        }
+
+        System.out.println("Getting data from local database");
+        return authentication.getName();
+    }
+}
