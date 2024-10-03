7bbfdd7589edc57bf7901febabf36133cc414ed0
yadav-deepakk <deepakyadav53882@gmail.com>
2024-10-03 23:48:07 +0530

    displaying message to front end about email verification done.

--- src/main/java/com/example/scm/controllers/AuthController.java
+++ src/main/java/com/example/scm/controllers/AuthController.java
@@ -16,6 +16,8 @@ import com.example.scm.forms.ForgetPassForm;
 import com.example.scm.models.Message;
 import com.example.scm.services.servicesImpl.UserServiceImpl;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping(path = "/auth")
 public class AuthController {
@@ -30,37 +32,42 @@ public class AuthController {
 
     @RequestMapping(path = "/verify-email", method = RequestMethod.GET)
     public String verifyEmail(@RequestParam String nonce, @RequestParam String email, @RequestParam String otp,
-            Model model) {
+            HttpSession session) {
 
         log.info("Doing email verification for :: {}", email);
 
         User user = userService.getUserByEmail(email).get();
-        if (user.isEmailVerified()) {
-            model.addAttribute("message", Message.builder()
-                    .messageType(MessageType.green)
-                    .messageContent("Your email has been already verified.")
-                    .build());
-            log.info("Email Already verified for user :: ", user.getEmail());
-        }
 
         Date otpIssueDate = user.getEmailOTPIssuedAt();
         float timeDiff = (float) Math.abs((new Date()).getTime() - otpIssueDate.getTime()) / (60 * 1000); // msec -> min
         String nonceInDB = user.getNonce();
         String otpInDB = user.getEmailOTP();
-        if (timeDiff < AppConstants.EMAIL_VERIFICATION_ALLOWED_DELAY_MIN
-                && nonce.equals(nonceInDB) && otp.equals(otpInDB)) {
-            user.setEmailVerified(true);
-            user.setEnabled(true);
-            userService.saveUser(user);
-            model.addAttribute("message",
-                    Message.builder().messageType(MessageType.green).messageContent("Your email verified.").build());
-            log.info("Email verification succeeded for user :: {}", user.getEmail());
+
+        if (timeDiff < AppConstants.EMAIL_VERIFICATION_ALLOWED_DELAY_MIN) {
+            if (nonce.equals(nonceInDB) && otp.equals(otpInDB)) {
+                user.setEmailVerified(true);
+                user.setEnabled(true);
+                userService.saveUser(user);
+                session.setAttribute("message",
+                        Message.builder().messageType(MessageType.green)
+                                .messageContent(
+                                        "Your email verification has been done. You can access SCM app now by logging in.")
+                                .build());
+                log.info("Email verification succeeded for user :: {}", user.getEmail());
+            } else {
+                session.setAttribute("message",
+                        Message.builder().messageType(MessageType.red)
+                                .messageContent("Invalid Link. Try to login and generate a new verification link.")
+                                .build());
+                log.info("Email verification failed for user :: {}", user.getEmail());
+            }
+        } else {
+            session.setAttribute("message",
+                    Message.builder().messageType(MessageType.red)
+                            .messageContent("Link expired. Try to login and generate a new verification link.")
+                            .build());
+            log.info("Email verification failed for user :: {}", user.getEmail());
         }
-
-        model.addAttribute("message",
-                Message.builder().messageType(MessageType.green).messageContent("Your email verified.").build());
-        log.info("Email verification failed for user :: {}", user.getEmail());
-
         return "user/verify-email";
     }
 
